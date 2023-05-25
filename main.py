46bc0cb52c75a76b4e5d5ee9b7b735035a2f644f
ibtissam01 <89752387+ibtissam01@users.noreply.github.com>
2023-05-25 15:48:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Mon application Streamlit")
 
 # Incorporer le tableau de bord Power BI
-st.components.v1.html(open('chemin/vers/votre/fichier.html', 'r', encoding='utf-8').read(), height=600)
+st.components.v1.html(open('html.pbit', 'r', encoding='utf-8').read(), height=600)
